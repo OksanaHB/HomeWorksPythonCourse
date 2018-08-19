db8fcd9b67b1e73626ea4a6d09939f4ecda84a3b
OksanaHB <Oksanahb@hotmail.com>
2018-08-19 13:07:24 +0300

    HomeWork 1

---
+++ HomeWorksPythonCourse/Task1.py
@@ -0,0 +1,6 @@
+user_name = input("What is your name? \n")
+user_age = input("How old are you? \n")
+user_address = input("Where are you live? \n")
+print("Hello,", user_name +"!")
+print("Your age is", user_age + ".")
+print("You live in", user_address + ".")
