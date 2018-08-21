529379cbb00280d501a1352897c45aa0710acea7
OksanaHB <Oksanahb@hotmail.com>
2018-08-21 15:49:36 +0300

    add new tasks

---
+++ CodeWars/1.py
@@ -0,0 +1,5 @@
+s=5
+if type(s) is str:
+    print("es un string")
+else:
+    print("no es string")
---
+++ CodeWars/reversing_words_in_string.py
@@ -0,0 +1,2 @@
+def reverse(text_to_reverse):
+    return ' '.join(list(reversed(str(text_to_reverse).split())))
