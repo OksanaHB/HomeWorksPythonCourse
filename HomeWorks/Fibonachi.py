1a034ad95271e25870c1377a848634e94d018fe1
OksanaHB <Oksanahb@hotmail.com>
2018-09-12 14:37:06 +0300

    Add some homeworks

--- HomeWorks/Fibonachi.py
+++ HomeWorks/Fibonachi.py
@@ -5,5 +5,8 @@ def fibonachi(n):
         return 1
     return fibonachi(n-2)+fibonachi(n-1)
    
-n=int(input())
-print(fibonachi(n))
+fibonachi_dict = {0:0, 1:1}
+def fibonachi_memo(n):
+    if not n in fibonachi_dict:
+        fibonachi_dict[n] = fibm(n-1) + fibm(n-2)
+    return fibonachi_dict[n]
