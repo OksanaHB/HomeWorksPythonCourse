067081a4c9b0187a393c870772272bd77334c712
OksanaHB <Oksanahb@hotmail.com>
2018-09-12 13:50:32 +0300

    Add some CodeWars changes

--- CodeWars/Fraction_class.py
+++ CodeWars/Fraction_class.py
@@ -1,7 +1,9 @@
-def mcm(num1,num2):
-    return  (num1 * num2) // mcd(num1,num2)
-def mcd(num1,num2):
-    return num1 if num2 == 0 else mcd(num2, num1%num2)
+def lcm(num1,num2):
+    """least common multiple"""
+    return  (num1 * num2) // gcd(num1,num2)
+def gcd(num1,num2):
+    """Greatest common divisor"""
+    return num1 if num2 == 0 else gcd(num2, num1%num2)
 class Fraction:
     def __init__(self, numerator, denominator):
         self.top = numerator
@@ -13,8 +15,6 @@ class Fraction:
     def __str__(self):
         return "%s/%s" % (self.top, self.bottom)
     def __add__(self, other):
-        fraction=mcm(self.bottom, other.bottom)
-        top = int(self.top*fraction/self.bottom+other.top*fraction/other.bottom)
-
-        return Fraction(top,fraction) if mcd(top,fraction) == 1 else Fraction(int(top/mcd(top,fraction)),int(fraction/mcd(top,fraction)))
-
+        fraction=lcm(self.bottom, other.bottom)
+        top = self.top*fraction//self.bottom+other.top*fraction//other.bottom
+        return Fraction(top,fraction) if gcd(top,fraction) == 1 else Fraction(top//gcd(top,fraction),fraction//gcd(top,fraction))
