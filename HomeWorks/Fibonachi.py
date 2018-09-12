4bf14cda2ef3c3070864ba542e65bbbf0ce21fd4
OksanaHB <Oksanahb@hotmail.com>
2018-09-12 14:42:07 +0300

    Add some homeworks

--- HomeWorks/Fibonachi.py
+++ HomeWorks/Fibonachi.py
@@ -1,4 +1,5 @@
 def fibonachi(n):
+    """ recurcive function por calculate fobonachi number """
     if n==1:
        return 0
     elif n==2:
@@ -6,7 +7,8 @@ def fibonachi(n):
     return fibonachi(n-2)+fibonachi(n-1)
    
 fibonachi_dict = {0:0, 1:1}
-def fibonachi_memo(n):
+def fibonachi_faster(n):
+    """faster version recurcive function por calculate fobonachi number """
     if not n in fibonachi_dict:
-        fibonachi_dict[n] = fibm(n-1) + fibm(n-2)
+        fibonachi_dict[n] = fibonachi_faster(n-1) + fibonachi_faster(n-2)
     return fibonachi_dict[n]
